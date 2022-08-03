661e8ac62b4f220dc5d806af1f790a344b863d1a
thomscoder <moyelysian@icloud.com>
2022-08-03 21:54:15 +0200

    refactor(cell.ts): refactored the way of rendering walls

--- client/src/maze/helpers/cell.ts
+++ client/src/maze/helpers/cell.ts
@@ -1,10 +1,17 @@
-import { ShikariCell } from "../utils/utils";
+import { ShikariCell } from "@maze/utils/utils";
 
 export default class Cell implements ShikariCell {
     public readonly posX: number;
     public readonly posY: number;
     // Delta represents the size of each cell
     private delta: number;
+    // Each cell starts with all 4 walls: top, right, bottom, left
+    private walls: Map<string, boolean> = new Map([
+        ["top", true], 
+        ["right", true], 
+        ["bottom", true], 
+        ["left", true]
+    ]);
 
     constructor(public x: number, public y: number, public _delta: number) {
         this.posX = x;
@@ -24,9 +31,29 @@ export default class Cell implements ShikariCell {
 
     private rect(x: number, y: number, delta: number, c: HTMLCanvasElement) {
         const ctx = c.getContext("2d");
+        
         ctx?.beginPath();
-        ctx?.rect(x, y, delta, delta);
         ctx!.strokeStyle = "#fff";
+        
+        const self = this;
+        renderWalls();
+        // ctx?.rect(x, y, delta, delta);
         ctx?.stroke();
+
+        //Draw each walls separately
+        function renderWalls() {
+            // top left corner
+            ctx?.moveTo(x, y);
+            if (self.walls.get("top")) ctx?.lineTo(x + delta, y);
+            // top right corner
+            ctx?.moveTo(x + delta, y);
+            if (self.walls.get("right")) ctx?.lineTo(x + delta, y + delta);
+            // bottom right corner
+            ctx?.moveTo(x + delta, y + delta);
+            if (self.walls.get("bottom")) ctx?.lineTo(x, y + delta);
+            // bottom left corner
+            ctx?.moveTo(x, y + delta);
+            if (self.walls.get("left")) ctx?.lineTo(x, y);
+        }
     }
 }
